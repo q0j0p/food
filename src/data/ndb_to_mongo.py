4f32e8858c2e232e7f9e872e1482e42d4a57962e
q0j0p <emailjamesc@gmail.com>
2017-03-14 09:00:21 -0700

    Extend functionality to lists other than food

--- src/data/ndb_to_mongo.py
+++ src/data/ndb_to_mongo.py
@@ -7,7 +7,30 @@ import pymongo
 import pdb
 BASE_URL = "https://api.nal.usda.gov/ndb/list"
 
-def get_usda_ndb_listdata(offset=0):
+
+class data_collector(object):
+    pass
+
+def get_usda_ndb_foodlist(offset=0, **kwargs):
+    params = {"lt":"f",
+              "sort":"id",
+              "max":"50",
+              "offset":str(offset),
+              "api_key":"PDddrH73wtpvf3gAtRyQUXVWlv80tON7RBFp2xlm"}
+    db = client.usda_ndb
+    coll = db.nutrient_list
+    print "start collection \n"
+    for n in xrange(0,99800,50): #184,022 food items in database; list of 50 at a time.
+      t = get_usda_ndb_listdata(offset=n)
+      print "status: {}\n".format(str(t))
+#      pdb.set_trace()
+      coll.insert_many(t.json()['list']['item'])
+      print "inserted {} entries\nstarting with {}\n".format(n,
+                                                            t.json()['list']['item'][0])
+      time.sleep(0)
+
+
+def get_usda_ndb_listdata(offset=0, **kwargs):
     params = {"lt":"f",
               "sort":"id",
               "max":"50",
@@ -20,12 +43,12 @@ if __name__ == "__main__":
     client = pymongo.MongoClient('mongodb://localhost:27017/')
     db = client.usda_ndb
     coll = db.food_list
-    for n in xrange(499500,184022,50): #184,022 food items in database; list of 50 at a time.
+    print "start collection \n"
+    for n in xrange(0,184022,50): #184,022 food items in database; list of 50 at a time.
       t = get_usda_ndb_listdata(offset=n)
+      print "status: {}\n".format(str(t))
 #      pdb.set_trace()
       coll.insert_many(t.json()['list']['item'])
-      print "inserted {} entries\nstarting with {}, status: \n".format(n,
-                                                            t.json()['list']['item'][0],
-                                                            str(t))
-
-      time.sleep(4)
+      print "inserted {} entries\nstarting with {}\n".format(n,
+                                                            t.json()['list']['item'][0])
+      time.sleep(0)
